ddf4ea37ffa96ab26f1219b04abd5af4eca23824
unknown <dummynoikofi@gmail.com>
2019-09-10 11:52:23 +0300

    Created class Lipa_na_Mpesa

---
+++ Lipa_na_Mpesa.py
@@ -0,0 +1,7 @@
+
+class Lipa_na_mpesa():
+    """Lipa_na_mpesa class initiates the online payment."""
+
+    def __init__(self):
+        super(Lipa_na_mpesa, self).__init__()
+        self.arg = arg
